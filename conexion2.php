1471c4d7aaf8b0908f3d9d5477e3655987ceb683
Nicolas <nicolas.carrasco.m@gmail.com>
2020-11-12 02:03:46 -0300

    migrar mysql 2

--- conexion2.php
+++ conexion2.php
@@ -4,5 +4,3 @@ $conn = mysqli_connect('us-cdbr-east-02.cleardb.com', 'b5dcd93157bee2', '0b557b0
 
 ?>
 
-
-mysql://b5dcd93157bee2:0b557b03@us-cdbr-east-02.cleardb.com/heroku_0530bad075bab9f?reconnect=true
