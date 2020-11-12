8feca8558bf7fce40e85c124acec357aab1a06b7
Nicolas <nicolas.carrasco.m@gmail.com>
2020-11-11 23:06:00 -0300

    nueva conexion

--- conexion.php
+++ conexion.php
@@ -1,11 +1,11 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$password ="";
+$servidor = "us-cdbr-east-02.cleardb.com";
+$usuario = "b891cdbe3ce634";
+$password ="6bc7691b";
 
 try {
  
-    $conn = new PDO("mysql:host=$servidor;dbname=bsale_test", $usuario, $password, 
+    $conn = new PDO("mysql:host=$servidor;dbname=heroku_21c0133d534c49a", $usuario, $password, 
     array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
